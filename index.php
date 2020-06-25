e212cfffca686f87d899014b38cb016f284f8f7b
HenLeiko <jioiiika1337@gmail.com>
2020-06-25 09:20:20 +0300

    make index page

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Slab:wght@400;500&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="nav__inner">
+                    <img src="img/maeve_logo_white.png" alt="Logo" class="nav__logo">
+                    <a href="" class="nav__link">Категории</a>
+                    <a href="" class="nav__link">Топ 100 фильмов</a>
+                    <input type="search" name="nav-search" placeholder="Поиск" id="search" class="nav__search">
+                    <input type="submit" value="Вход/Регистрация" class="nav__btn">
+                </div>
+            </div>
+        </nav>
+        <div class="banner">
+            <div class="container">
+                <div class="banner__inner">
+                    <img src="img/banner.png" alt="Banner" class="banner__img">
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="films-tile">
+            <div class="container">
+                <div class="films-tile__inner">
+                    <div class="tile">
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                        <div class="tile__item">
+                            <img src="img/avatar.png" alt="Avatar" class="tile__img">
+                        </div>
+                    </div>
+                    <div class="side-bar">
+                        <div class="side-bar__item">
+                            <div class="side-bar__title">Lorem, ipsum:</div>
+                            <ul class="side-bar__list">
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                            </ul>
+                        </div>
+                        <div class="side-bar__item">
+                            <div class="side-bar__title">Lorem, ipsum:</div>
+                            <ul class="side-bar__list">
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                                <li class="side-bar__link"><a href="">Lorem</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="video-slider">
+            <div class="container">
+                <div class="section-title">
+                    <div class="section-title__block">
+                        Популярное
+                    </div>
+                    <div class="section-title__triangle"></div>
+                    <div class="section-title__line"></div>
+                </div>
+                <div class="slide-cards">
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="video-slider">
+            <div class="container">
+                <div class="section-title">
+                    <div class="section-title__block">
+                        Популярное
+                    </div>
+                    <div class="section-title__triangle"></div>
+                    <div class="section-title__line"></div>
+                </div>
+                <div class="slide-cards">
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                    <div class="slide-cards__card">
+                        <img src="img/slide_1.png" alt="slide" class="slide-cards__img">
+                        <div class="slide-cards__desc">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="footer__inner">
+                <div class="footer__information">
+                    <div class="footer__logo">
+                        <img src="img/maeve_logo_white.png" alt="" class="footer__img">
+                    </div>
+                    <div class="footer__down">
+                    <div class="footer__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                        Risus commodo viverra maecenas accumsan lacus vel facilisis. </div>
+                        <div class="footer__contacts">
+                    <div class="contacts__up">
+                        <div class="contacts__phone">Phone: +7 (900)-255-25-25</div>
+                        <div class="contacts__email">Email: email@mail.ru</div>
+                    </div>
+                    <div class="contscts__down">
+                        <a href="https://www.instagram.com/henleiko/"><img src="img/instagram_ico.png" alt="instagram" class="contacts__soc"></a>
+                        <a href="https://www.facebook.com/profile.php?id=100008350997692"><img src="img/facebook_ico.png" alt="facebook" class="contacts__soc"></a>
+                        <a href="https://twitter.com/HenLeiko"><img src="img/twitter_ico.png" alt="twitter" class="contacts__soc"></a>
+                    </div>
+                </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
