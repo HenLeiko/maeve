a5f629338f8aafcdd5baeb46b77f3fbfa92eade4
HenLeiko <jioiiika1337@gmail.com>
2020-06-26 04:45:59 +0300

    make open film page

---
+++ film.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Slab:wght@400;500&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="nav__inner">
+                    <img src="img/maeve_logo_white.png" alt="Logo" class="nav__logo">
+                    <a href="" class="nav__link">Категории</a>
+                    <a href="" class="nav__link">Топ 100 фильмов</a>
+                    <input type="search" name="nav-search" placeholder="Поиск" id="search" class="nav__search">
+                    <input type="submit" value="Вход/Регистрация" class="nav__btn">
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="film">
+            <div class="container">
+                <div class="film__inner">
+                    <div class="film__banner"><img src="img/side_banner.png" alt="side-banner" class="film__img"></div>
+                    <div class="film__info">
+                        <div class="title">Kill Bill</div>
+                        <div class="film__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                            laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                            quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+                            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                            consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                            dolore magnam aliquam quaerat voluptatem.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="stats">
+            <div class="container">
+                <div class="stats__inner">
+                    <div class="title__spoiler">Статистика просмотра фильма
+                        <div class="wipe-btn">
+                            <img src="img/wipe.png" alt="wipe-btn" class="wipe-btn__img">
+                        </div>
+                    </div>
+                    <div class="stats__graph">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <div class="footer__inner">
+                <div class="footer__information">
+                    <div class="footer__logo">
+                        <img src="img/maeve_logo_white.png" alt="" class="footer__img">
+                    </div>
+                    <div class="footer__down">
+                        <div class="footer__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                            Risus commodo viverra maecenas accumsan lacus vel facilisis. </div>
+                        <div class="footer__contacts">
+                            <div class="contacts__up">
+                                <div class="contacts__phone">Phone: +7 (900)-255-25-25</div>
+                                <div class="contacts__email">Email: email@mail.ru</div>
+                            </div>
+                            <div class="contscts__down">
+                                <a href="https://www.instagram.com/henleiko/"><img src="img/instagram_ico.png"
+                                        alt="instagram" class="contacts__soc"></a>
+                                <a href="https://www.facebook.com/profile.php?id=100008350997692"><img
+                                        src="img/facebook_ico.png" alt="facebook" class="contacts__soc"></a>
+                                <a href="https://twitter.com/HenLeiko"><img src="img/twitter_ico.png" alt="twitter"
+                                        class="contacts__soc"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
