77329b78644aec18227d5f27ea0341d908989f60
HenLeiko <jioiiika1337@gmail.com>
2020-06-28 11:22:48 +0300

    make admin-list functions for admin panel

---
+++ temp/adminlist.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'db.php';
+$role = 'Администратор';
+$users = R::find('users', 'rol LIKE ?', ["%$role%"]);
+
+foreach ($users as $new): ?>
+<tr class="user-list__user">
+    <td><?=$new['id']?></td>
+    <td><?=$new['login']?></td>
+    <td><?=$new['password']?></td>
+    <td><?=$new['status']?></td>
+    <td><?=$new['rol']?></td>
+</tr>
+<?php endforeach; ?>
