07bfa8df22ef04d4a057cd9638dec46dbb987ac7
HenLeiko <jioiiika1337@gmail.com>
2020-06-28 11:21:53 +0300

    add functions for register

--- register.php
+++ register.php
@@ -1,10 +1,39 @@
 <?php
-require 'db.php';
-$data = json_decode(file_get_contents('php://input'), true);
-$response = [];
+session_start();
+require_once 'db.php';
+include 'wrigthlog.php';
+date_default_timezone_set('UTC');
+$data = $_POST;
+$err = [];
+$date = date('l jS \of F Y');
+print_r($data);
+require 'validation.php';
 
-$user = R::dispense('users');
-$user->email = $data['email'];
-$user->login = $data['login'];
-$user->password = $data['password'];
-R::store($user);
+if (empty($err)) {
+    $users = R::dispense('users');
+    $profile = R::dispense('profiles');
+    $users->email = $data['email'];
+    $users->login = $data['login'];
+    $users->password = $data['password'];
+    $users->reg_date = $date;
+    $users->status = 'active';
+    $users->rol = 'Пользователь';
+
+    $profile->user = $users;
+    $profile->reg_date = $date;
+    $profile->last_online = $date;
+    R::store($users);
+    R::store($profile);
+    $user = R::findOne('users', 'login = ?', [$data['login']]);
+
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['login'] = $data['login'];
+    $_SESSION['status'] = 'active';
+    
+    $text = 'Регистрация успешно пройдена пользователем: '. $_SESSION['login'];
+    logFile($text);
+    $code = 201;
+    echo $code;
+} else {
+    echo array_shift($err);
+}
